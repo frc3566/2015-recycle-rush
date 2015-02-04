cc59707c0aef2f0c9dfc246f553d74f9a7c6e6dd
frc3566-teammate <battis+frc3566@gmail.com>
2015-02-04 14:41:50 -0500

    Update on the Elevator

--- Official_Competition_Code_2015/src/org/usfirst/frc3566/Official_Competition_Code_2015/RobotConstants.java
+++ Official_Competition_Code_2015/src/org/usfirst/frc3566/Official_Competition_Code_2015/RobotConstants.java
@@ -3,4 +3,8 @@ package org.usfirst.frc3566.Official_Competition_Code_2015;
 public class RobotConstants {
 	public static double Robot_Mecanum_RunSpeed_Coefficient = 0.65;
 	public static double Robot_Mecanum_TurnSpeed_Coefficient = 0.80;
+	public static double Robot_Elevator_RaiseSpeed_A_Coefficient = -1;
+	public static double Robot_Elevator_LowerSpeed_A_Coefficient = 1;
+	public static double Robot_Elevator_RaiseSpeed_B_Coefficient = -1;
+	public static double Robot_Elevator_LowerSpeed_B_Coefficient = 1;
 }
